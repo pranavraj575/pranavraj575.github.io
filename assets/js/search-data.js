171e41fe888578f90e760144b565da6015fec6b7
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-19 00:32:59 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@852ffc54bc42ecef0ca18c101647d23e0cca4c39 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,21 +427,16 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+      },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-making-a-website",
+          title: 'Making a website',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
