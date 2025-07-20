6c635147ba9b18c23f005dba5e5857e4a67a8f9c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-20 01:35:06 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@453e8a15d1af2f7ef4eed2e8c09ae34d00563255 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-i-class-fa-fa-fw-fa-home-i-nbsp-home",
-    title: "<i class="fa fa-fw fa-home"></i>&nbsp;Home",
+    title: "<i class="fa fa-fw fa-home"></i>&nbsp;home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
