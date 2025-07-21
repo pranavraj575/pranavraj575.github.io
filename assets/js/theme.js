b430bcaea967bcd3775d32005ae32d33ebf68e8c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-21 19:25:07 +1000

    is this the issue?

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -260,7 +260,7 @@ let transTheme = () => {
 // "system". Default is "system".
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system" && themeSetting != "abomination") {
     themeSetting = "system";
   }
   return themeSetting;
