9c5988ea30cfbbc7f1858cd74560f9baccc9a79c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-22 12:51:01 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@5528c295a540d2997770dc3e412c95105acf6679 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,17 +76,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/cat/";
           
         },
-      },{id: "post-roos",
-        
-          title: "Roos",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/roo/";
-          
-        },
       },{id: "post-meevis",
         
           title: "Meevis",
