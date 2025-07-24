fd9a07c8b4d05b4fae71d467f07fa415d8c6fa6a
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-24 21:18:03 +1000

    try adding categories to search

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -343,4 +343,24 @@ ninja.data = [
       },
     },
   {%- endif -%}
+
+  {%- if site.posts_in_search -%}
+    {%- for post in site.posts -%}
+      {%- for category in post.categories -%}
+        {
+          {%- assign title = category | newline_to_br | replace: "<br />", " " | replace: "<br/>", " " | strip_html | strip_newlines | escape | strip -%}
+          id: "category-aminals-{{ title | slugify }}",
+          title: '{{ title | escape | emojify | truncatewords: 13 }}',
+          description: "{{ category | strip_html | strip_newlines | escape | strip }}",
+          section: "Post categories",
+          {%- unless item.inline -%}
+            handler: () => {
+              window.location.href = "{{ aminals | append: '/category/' | append {{ category }} | relative_url }}";
+            },
+          {%- endunless -%}
+        },
+      {%- endfor -%}
+    {%- endfor -%}
+  {%- endif -%}
+
 ];
