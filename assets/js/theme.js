678c165bf27663b88d660297878ecd840b6abbc3
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-25 21:30:37 +1000

    toggle elements when goose is set

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -32,6 +32,7 @@ let setThemeSetting = (themeSetting) => {
   if (themeSetting=="light"){
      setGooseSetting("serious");
   }
+  gooseActivation();
 };
 
 // Change the goose setting and apply the goose.
@@ -288,6 +289,27 @@ let determineGooseSetting = () => {
     gooseSetting = "serious";
   }
   return gooseSetting;
+};
+
+// beauty will be revealed
+let gooseActivation = () => {
+  var silly_geese = document.getElementsByClassName("silly-goose");
+  var goose_setting = document.documentElement.getAttribute("goose-setting");
+  for (var i = 0; i < silly_geese.length; i++) {
+    if (goose_setting == "silly"){
+      silly_geese[i].style.display = "block";
+    } else {
+      silly_geese[i].style.display = "none";
+    }
+  }
+  var serious_geese = document.getElementsByClassName("serious-goose");
+  for (var i = 0; i < serious_geese.length; i++) {
+    if (goose_setting == "silly"){
+      serious_geese[i].style.display = "none";
+    } else {
+      serious_geese[i].style.display = "block";
+    }
+  }
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
