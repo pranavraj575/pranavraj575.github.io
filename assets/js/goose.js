45154648c6b352c30dff3f7b3443972536b5e5ec
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-07-30 22:01:35 +1000

    try conditional reveals at number of secrets, set total secrets to 2

--- assets/js/goose.js
+++ assets/js/goose.js
@@ -14,7 +14,6 @@ let setStepsFound = (steps) => {
   document.documentElement.setAttribute("secrets-found", String(steps));
   let gooseSetting = determineGooseSetting(steps);
   setGooseSetting(gooseSetting);
-  gooseActivation();
 };
 
 // Set steps found to max(stored steps found,steps), apply goose
@@ -24,9 +23,11 @@ let increaseStepsTo = (steps) => {
   }
 };
 
+let numSecrets = 2;
+
 // Determine the goose state
 let determineGooseSetting = (steps) => {
-  if (steps > 0) {
+  if (steps >= numSecrets) {
     return "silly";
   }
   else{
@@ -45,9 +46,43 @@ let determineStepsFound = () => {
 };
 
 // beauty will be revealed
+// label something silly-goose if it is only revealed when all secrets found
+// label something serious-goose if it is only hidden when all secrets found
+// label something reveal-at-<num> if it is revealed after finding <num> secrets
+// label something hide-at-<num> if it is hidden after finding <num> secrets
+// silly-goose and serious-goose are equivalent to reveal-at-<numSecrets> and hide-at-<numSecrets>
+// if something is labeled reveal-at-<a> and hide-at-<b>, (note, a<b)
+//  it will be hidden with secrets < a, revealed at a<= secrets < b, and hidden with secrets>=b
+
 let gooseActivation = () => {
+  var secrets_found = determineStepsFound();
+  for (var secrets=0; secrets <= numSecrets; secrets++){
+    // iterate through everything that is revealed after a certian number of secrets
+    var silly_geese = document.getElementsByClassName("reveal-at-"+String(secrets));
+    for (var i = 0; i < silly_geese.length; i++) {
+      if(secrets <= secrets_found){
+        // if the number of secrets found is at least the number of secrets needed, we will reveal silly_geese[i]
+        silly_geese[i].style.display = "block";
+      }
+      else{
+        // otherwise, we must hide this goose
+        silly_geese[i].style.display = "none";
+      }
+    }
+
+    // iterate through everything that is hidden after a certian number of secrets
+    var serious_geese = document.getElementsByClassName("hide-at-"+String(secrets));
+    for (var i = 0; i < serious_geese.length; i++) {
+      if(secrets <= secrets_found){
+        // if the number of secrets found is at least the number of secrets needed, we will hide serious_geese[i]
+        serious_geese[i].style.display = "none";
+      }
+    }
+  }
+
   var silly_geese = document.getElementsByClassName("silly-goose");
   var goose_setting = document.documentElement.getAttribute("goose-setting");
+
   for (var i = 0; i < silly_geese.length; i++) {
     if (goose_setting == "silly"){
       silly_geese[i].style.display = "block";
