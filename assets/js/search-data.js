fcb0750b4d0886debdb021f87eca9bc2c335a868
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-08-02 13:25:16 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@cb69883eeb1bd456ec6ea8d25539e68892729f3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -136,6 +136,9 @@ ninja.data = [{
       section: 'Theme',
       handler: () => {
         setThemeSetting("light");
+        // BLINDING
+        // only triggers when the toggle button is hit (or when searched for), rather than whenever light theme is on
+        setStepsFound(0);
       },
     },
     {
