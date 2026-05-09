c7ad219ca2977518e67085f9934f87439ea4f27c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-05-09 15:43:11 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@149bcb11cefe3022d0cbc00389ebbe889beccf50 🚀

--- python_scripts/repos.py
+++ python_scripts/repos.py
@@ -79,14 +79,18 @@ for repo in data["github_repos"]:
         while "  " in item:
             item = item.replace("  ", " ")
         languages.append(item)
-    if len(languages)==1:
-        width=12
-    elif len(languages)==3 or len(languages)>4:
-        width=4
+    if len(languages) == 1:
+        width = 12
+    elif len(languages) == 3 or len(languages) > 4:
+        width = 4
     else:
-        width=6
+        width = 6
 
-    languages_html = f'<div class="list-groups"> <div class="list-group col-md-{width}" style="margin-bottom:0px">' + f'</div><div class="list-group col-md-{width}" style="margin-bottom:0px">'.join(languages) + "</div></div>"
+    languages_html = (
+        f'<div class="list-groups"> <div class="list-group col-md-{width}" style="margin-bottom:0px">'
+        + f'</div><div class="list-group col-md-{width}" style="margin-bottom:0px">'.join(languages)
+        + "</div></div>"
+    )
 
     full_thing = (
         repo_html.replace("TITLE", repo)
