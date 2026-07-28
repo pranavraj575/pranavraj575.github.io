b54856b5bc78a7d6c559f74e3e6cd7a3a7d8dce4
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-07-28 17:45:08 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@8e2ef9ccbfc0fb6c5dd0d3c57729fc0f25c33fbf 🚀

---
+++ python_scripts/recursive_ss.py
@@ -0,0 +1,38 @@
+from playwright.sync_api import sync_playwright
+import time
+import os
+
+
+def ctrl(k):
+    page.keyboard.down("Control")
+    page.keyboard.press(k)
+    page.keyboard.up("Control")
+
+
+def press(keys, delay=0.1):
+    for k in keys:
+        page.keyboard.press(k)
+        time.sleep(delay)
+
+
+DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+with sync_playwright() as p:
+    # open page
+    browser = p.firefox.launch()
+    page = browser.new_page()
+    page.goto("https://pranavraj575.github.io/repos")
+
+    # turn on dark mode
+    ctrl("K")
+    press("dark")
+    page.keyboard.press("Enter")
+
+    # scroll to self reference
+    # thing=page.get_by_text("You're looking at it")
+    thing = page.get_by_text("pranavraj575/pranavraj575.github.io")
+    thing.scroll_into_view_if_needed()
+    # let any animations pass
+    time.sleep(1)
+
+    page.screenshot(path=os.path.join(DIR, "assets", "img", "stuff", "repo_screenshot.png"), full_page=False)
+    browser.close()
