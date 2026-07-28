b432716f1fb8a6168a5a3ea0cc95a90398494d02
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-07-28 17:54:57 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@c6b6b55211a5c229ce78d60809353ec029087acc 🚀

--- python_scripts/recursive_ss.py
+++ python_scripts/recursive_ss.py
@@ -12,11 +12,13 @@ def press(keys, delay=0.1):
 DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with sync_playwright() as p:
     # open page
+    print("opening page")
     browser = p.firefox.launch()
     page = browser.new_page()
     page.goto("https://pranavraj575.github.io/repos")
 
     # turn on dark mode
+    print("darkening the mode")
     page.keyboard.down("Control")
     page.keyboard.press("k")
     page.keyboard.up("Control")
@@ -24,11 +26,13 @@ with sync_playwright() as p:
     page.keyboard.press("Enter")
 
     # scroll to self reference
+    print("scrolling to recursion")
     thing = page.get_by_text("pranavraj575/pranavraj575.github.io")
     thing.scroll_into_view_if_needed()
 
     # let any animations pass
     time.sleep(1)
 
+    print("screening my shot")
     page.screenshot(path=os.path.join(DIR, "assets", "img", "stuff", "repo_screenshot.png"), full_page=False)
     browser.close()
