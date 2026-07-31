280222fb4b9e1e63a684595c1fee6d678d235433
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-07-31 01:54:03 -0400

    formatting

--- python_scripts/use_check.py
+++ python_scripts/use_check.py
@@ -1,4 +1,6 @@
-import argparse,os
+import argparse
+import os
+
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument(
     "--file",
@@ -12,39 +14,42 @@ PARSER.add_argument(
     required=True,
     help="folders to check references in",
 )
-failed_formats=set()
+failed_formats = set()
 args = PARSER.parse_args()
-def recurse_search(path,string,ignore=None):
+
+
+def recurse_search(path, string, ignore=None):
 
     if ignore is None:
-        ignore=[]
+        ignore = []
     if os.path.isdir(path):
-        return any(recurse_search(os.path.join(path,t),string,ignore) for t in os.listdir(path))
+        return any(recurse_search(os.path.join(path, t), string, ignore) for t in os.listdir(path))
     else:
-        if os.path.basename(path) in ignore or any(path.endswith(ext) for ext in ('.gif','.pdf',".mp3",".ttf",".woff",".woff2",".wav",".eot",'.png','.ico','.jpg','.mp4')) :
+        if os.path.basename(path) in ignore or any(
+            path.endswith(ext)
+            for ext in (".gif", ".pdf", ".mp3", ".ttf", ".woff", ".woff2", ".wav", ".eot", ".png", ".ico", ".jpg", ".mp4")
+        ):
             return False
         try:
-            with open(path,'r',encoding="utf-8") as f:
+            with open(path, "r", encoding="utf-8") as f:
                 return string in f.read()
         except UnicodeDecodeError:
-            ext=path[path.rindex('.'):]
+            ext = path[path.rindex(".") :]
             if ext not in failed_formats:
                 print("FAILED TO READ:", ext)
             failed_formats.add(ext)
 
 
-files=list(args.file)
-i=0
-while i<len(files):
+files = list(args.file)
+i = 0
+while i < len(files):
     if os.path.isdir(files[i]):
-        f=files.pop(i)
-        files.extend(os.path.join(f,t) for t in os.listdir(f))
+        f = files.pop(i)
+        files.extend(os.path.join(f, t) for t in os.listdir(f))
     else:
-        i+=1
+        i += 1
 
-used=list(filter(lambda f : any(recurse_search(dd,os.path.basename(f),[os.path.basename(f)]) for dd in args.folders), files))
-unused=list(set(files).difference(used))
-print("USED")
-print(used)
-print("UNUSED")
-print(unused)
+used = list(filter(lambda f: any(recurse_search(dd, os.path.basename(f), [os.path.basename(f)]) for dd in args.folders), files))
+unused = sorted(list(set(files).difference(used)), key=lambda f: files.index(f))
+print("USED", *used, sep="\n\t")
+print("UNUSED", *unused, sep="\n\t")
