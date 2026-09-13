6b2c0219ae439342de61d489a4a5a03c1b94f994
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-09-13 03:53:40 +0000

    Deploying to gh-pages from @ pranavraj575/pranavraj575.github.io@353a6fd43e019c1dc27077899383b5bcc474cb54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -214,17 +214,6 @@ ninja.data = [{
             window.location.href = "/aminals/2026/baby_dragon/";
           
         },
-      },{id: "post-sssnake",
-        
-          title: "sssnake",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/aminals/2026/snake/";
-          
-        },
       },{id: "post-molly",
         
           title: "molly",
