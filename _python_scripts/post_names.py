bbadb5c96db9feb8275c52d126d2f426ef4c2724
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2026-09-17 10:05:30 -0400

    formatting

--- _python_scripts/post_names.py
+++ _python_scripts/post_names.py
@@ -1,4 +1,8 @@
-import os,yaml,argparse,datetime,shutil
+import os
+import yaml
+import argparse
+import datetime
+import shutil
 
 
 PARSER = argparse.ArgumentParser()
@@ -16,15 +20,15 @@ PARSER.add_argument(
 args = PARSER.parse_args()
 for fn in args.files:
     with open(fn) as f:
-        d=next(yaml.safe_load_all(f))
-    assert 'date' in d
-    date:datetime.datetime=d['date']
-    date_str=date.strftime('%Y-%m-%d-')
+        d = next(yaml.safe_load_all(f))
+    assert "date" in d
+    date: datetime.datetime = d["date"]
+    date_str = date.strftime("%Y-%m-%d-")
 
     if not os.path.basename(fn).startswith(date_str):
-        src,tgt=fn,os.path.join(os.path.dirname(fn),date_str+os.path.basename(fn))
+        src, tgt = fn, os.path.join(os.path.dirname(fn), date_str + os.path.basename(fn))
         if args.rename:
             print(f'"{src}" will be renamed "{tgt}"')
-            shutil.move(src,tgt)
+            shutil.move(src, tgt)
         else:
             print(f'"{src}" should be renamed "{tgt}"')
